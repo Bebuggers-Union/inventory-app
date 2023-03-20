3499e01ed8468a9b61744a0c26e1cff52431df33
Ciaraerica23 <ciaraestafford@gmail.com>
2023-03-20 12:34:14 -0400

    added item model

--- server/models/index.js
+++ server/models/index.js
@@ -6,7 +6,15 @@ const Sauce = sequelize.define("sauces", {
   image: Sequelize.STRING,
 });
 
+const Items = sequelize.define("items", {
+  title:Sequelize.STRING,
+  price:Sequelize.NUMBER,
+  description:Sequelize.STRING,
+  category:Sequelize.STRING,
+  image:Sequelize.STRING
+});
+
 module.exports = {
   db: sequelize,
-  Sauce,
+  Sauce, Items
 };
