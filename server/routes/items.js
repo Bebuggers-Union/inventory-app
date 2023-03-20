9018e649001f617c7b794b80360cfa50ebf5cbf1
Anderson Tiban <andytiban12@gmail.com>
2023-03-20 12:47:27 -0400

    created item.js route, added get routes

---
+++ server/routes/items.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+ //Import Items table
+const Items = require("pathOfItemModelHere")
+
+//GET / items
+router.get("/", async (request, response) =>{
+    try{
+        const items = await Items.findAll(); //gets all items
+        response.send(items)
+    }catch(error){
+        console.log(error);
+    }
+})
+
+//GET one individual item
+router.get("/:id", async(request, response)=>{
+    try{
+        const oneItem = await Items.findByPk(req.params.id)
+        response.json(oneItem)
+    } catch(error){
+        console.log(error)
+    }
+})
+
+
+
+
+module.exports = router;
