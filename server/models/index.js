84ffe53cf3efbb288e865f19a64d757bcea619d6
Ciaraerica23 <ciaraestafford@gmail.com>
2023-03-20 12:39:54 -0400

    added edited item model

--- server/models/index.js
+++ server/models/index.js
@@ -6,15 +6,8 @@ const Sauce = sequelize.define("sauces", {
   image: Sequelize.STRING,
 });
 
-const Items = sequelize.define("items", {
-  title:Sequelize.STRING,
-  price:Sequelize.NUMBER,
-  description:Sequelize.STRING,
-  category:Sequelize.STRING,
-  image:Sequelize.STRING
-});
 
 module.exports = {
   db: sequelize,
-  Sauce, Items
+  Sauce
 };
---
+++ server/models/item.js
@@ -0,0 +1,13 @@
+const {Sequelize} = require('sequelize')
+const {sequelize} = require('../db')
+
+const Items = sequelize.define("items", {
+    title:Sequelize.STRING,
+    price:Sequelize.NUMBER,
+    description:Sequelize.STRING,
+    category:Sequelize.STRING,
+    image:Sequelize.STRING
+  });
+  
+module.exports= 
+    Items
