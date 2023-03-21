52c46d126829c00c62ce5dae375fdbe71025fb13
colledwards <colledwards@expediagroup.com>
2023-03-21 10:43:20 -0700

    fixed typo that broke everything :(

--- public/react/components/ItemList.js
+++ public/react/components/ItemList.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Item } from './Item'
 
-export const ItemsList = ({ items }) => {
+export const ItemList = ({ items }) => {
     return (
         <>
             {items.map((item, idx) => {
