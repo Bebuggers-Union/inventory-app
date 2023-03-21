3f9a975840b543415b74ff5ec4fbb08df8e68b77
colledwards <colledwards@expediagroup.com>
2023-03-21 10:57:17 -0700

    typo

--- public/react/components/Item.js
+++ public/react/components/Item.js
@@ -7,7 +7,7 @@ export const Item = (props) => {
                 <h3 className="item-title">{props.item.title}</h3>
                 <p>{props.item.category}</p>
                 <p>{props.item.price}</p>
-                <img src={props.item.image} alt={props.item.name} />
+                <img src={props.item.image} alt={props.item.title} />
                 <p className="item-desc">{props.item.description}</p>
                 <button>Back</button>
             </div>
