810412d3dde41a9fc350cac8d8cd0a8b7eca3286
aamcodes <aaronmendezcodes@gmail.com>
2023-03-21 14:13:14 -0400

    Added a new folder for the Form components and it's respective container, along with child components to build the foundation for the create new User and Item forms

---
+++ public/react/components/Forms/FormsContainer.js
@@ -0,0 +1,5 @@
+import React, { useState } from 'react'
+
+export const FormsContainer = () => {
+    return <div>FormsContainer</div>
+}
---
+++ public/react/components/Forms/NewItemForm.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const NewItemForm = () => {
+    return <div>NewItemForm</div>
+}
