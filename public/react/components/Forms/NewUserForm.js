fd7306d2dd9e1313a30004e5a48efa97c09b85d6
aamcodes <aaronmendezcodes@gmail.com>
2023-03-21 20:37:23 -0400

    Added some labels for the form

--- public/react/components/Forms/NewUserForm.js
+++ public/react/components/Forms/NewUserForm.js
@@ -14,27 +14,36 @@ export const NewUserForm = ({
                 ? serverErrors.map((err, idx) => <div key={idx}>{err}</div>)
                 : null}
             <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    name="name"
-                    placeholder="Enter Name"
-                    value={userForm.name}
-                    onChange={handleChange}
-                />
-                <input
-                    type="text"
-                    name="username"
-                    placeholder="Enter Username"
-                    value={userForm.username}
-                    onChange={handleChange}
-                />
-                <input
-                    type="text"
-                    name="password"
-                    placeholder="Enter Password"
-                    value={userForm.password}
-                    onChange={handleChange}
-                />
+                <label>
+                    Name
+                    <input
+                        type="text"
+                        name="name"
+                        placeholder="Enter Name"
+                        value={userForm.name}
+                        onChange={handleChange}
+                    />
+                </label>
+                <label>
+                    Username
+                    <input
+                        type="text"
+                        name="username"
+                        placeholder="Enter Username"
+                        value={userForm.username}
+                        onChange={handleChange}
+                    />
+                </label>
+                <label>
+                    Password
+                    <input
+                        type="text"
+                        name="password"
+                        placeholder="Enter Password"
+                        value={userForm.password}
+                        onChange={handleChange}
+                    />
+                </label>
                 <button>Submit</button>
             </form>
         </div>
